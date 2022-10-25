09674da978ed93584d985232a2ef937cf08eade9
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-10-25 19:34:38 +0300

    commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -15,3 +15,11 @@ export type DispatchType = typeof store.dispatch
 // store.subscribe(() => {
 // 	saveState<{ app: { counter: number } }>('counter', { app: { counter: store.getState().app.counter } })
 // })
+
+
+// const store = createStore(rootReducer, applyMiddleware(thunk))
+// type RootState = ReturnType<typeof store.getState>
+// type AppDispatch = ThunkDispatch<RootState, unknown, ActionsType>
+// type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, ActionsType>
+// const useAppDispatch = () => useDispatch<AppDispatch>()
+// const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
