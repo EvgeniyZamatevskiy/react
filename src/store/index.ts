195f0370856b23619c87a19f91b76cae585b7266
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-11-09 09:59:12 +0300

    commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { saveState, restoreState } from "services"
+import { setToLocalStorage, getFromLocalStorage } from "services"
 import appSlice from "./slices/app"
 
 export const store = configureStore({
   reducer: {
     app: appSlice,
   },
-  //preloadedState: restoreState('counter', {})
+  //preloadedState: getFromLocalStorage('counter', {})
 })
 
 export type RootStateType = ReturnType<typeof store.getState>
 export type DispatchType = typeof store.dispatch
 
 // store.subscribe(() => {
-// 	saveState<{ app: { counter: number } }>('counter', { app: { counter: store.getState().app.counter } })
+// 	setToLocalStorage<{ app: { counter: number } }>('counter', { app: { counter: store.getState().app.counter } })
 // })
 
 
